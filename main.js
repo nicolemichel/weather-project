e35aaabcf66ebefb72b795943f68cffb86fd0df2
Nicole Michel <45564767+nicolemichel@users.noreply.github.com>
2021-04-24 21:27:59 -0400

    Start of forecast

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
 // current date and time //
-let today = new Date();
-
-let weekdays = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday"
-];
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-
-let day = weekdays[today.getDay() - 1];
-let month = months[today.getMonth()];
-let date = today.getDate();
-let year = today.getFullYear();
-let hour = today.getHours();
-let minutes = today.getMinutes();
-
-if (hour < 10) {
-  hour = `0${hour}`;
+function formatDate(timestamp) {
+  let today = new Date(timestamp);
+  
+  let weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  
+  let day = weekdays[today.getDay() - 1];
+  let month = months[today.getMonth()];
+  let date = today.getDate();
+  let year = today.getFullYear();
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  
+  document.querySelector("#current-day").innerHTML = `${day}`;
+  document.querySelector(
+    "#current-full-date"
+  ).innerHTML = `${month} ${date}, ${year}`;
+  document.querySelector("#current-time").innerHTML = `${hour}:${minutes}`;
 }
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-document.querySelector("#current-day").innerHTML = `${day}`;
-document.querySelector("#current-full-date").innerHTML = `${month} ${date}, ${year}`;
-document.querySelector("#current-time").innerHTML = `${hour}:${minutes}`;
 
 // ºC click button change to ºF //
 let unitBtn = document.querySelector("#unit-btn");
+let unit = "metric";
 let clickCount = 0;
 let currentTemp = null;
 let currentWind = 0;
@@ -59,11 +64,12 @@ function changeUnit(event) {
     document.querySelector("#wind").innerHTML = `${currentWind} km/h`;
   } else {
     unitBtn.innerHTML = `ºF`;
-    document.querySelector("#current-temp").innerHTML = currentTemp;
-    let fahrenheitTemp = Math.round((currentTemp * 9) / 5 + 32);
-    document.querySelector("#current-temp").innerHTML = `${fahrenheitTemp}`;
+    //document.querySelector("#current-temp").innerHTML = currentTemp;
+    //let fahrenheitTemp = Math.round((currentTemp * 9) / 5 + 32);
+    //document.querySelector("#current-temp").innerHTML = `${fahrenheitTemp}`;
     let milesWind = Math.round(currentWind / 1.609);
     document.querySelector("#wind").innerHTML = `${milesWind} mph`;
+    unit = "imperial";
   }
 }
 
@@ -84,11 +90,9 @@ function showSearched(response) {
   document.querySelector("#current-temp").innerHTML = currentTemp;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   currentWind = Math.round(response.data.wind.speed);
-  document.querySelector("#wind").innerHTML = Math.round(
-    currentWind
-  ) + ` km/h`;
+  document.querySelector("#wind").innerHTML = Math.round(currentWind) + ` km/h`;
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document
     .querySelector("#current-icon")
     .setAttribute(
@@ -98,19 +102,23 @@ function showSearched(response) {
   document
     .querySelector("#current-icon")
     .setAttribute("alt", response.data.weather[0].description);
-    document.querySelector("#min-temp").innerHTML = Math.round(
-      response.data.main.temp_min
-    );
-    document.querySelector("#max-temp").innerHTML = Math.round(
-      response.data.main.temp_max
-    );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+
+  formatDate();
+
+  getForecast(response.data.coord);
 }
 
 searchBtn.addEventListener("click", showCity);
 
 function search(city) {
   let apiKey = "7188b6a77c9693ed94470114f98e8761";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(showSearched);
 }
 
@@ -139,6 +147,61 @@ function getCoords() {
 }
 
 locationBtn.addEventListener("click", getCoords);
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `   
+        <!--day 2 start-->
+          <div class="col-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="col-12">
+                  <p class="card-text">${formatDate(forecastDay.dt)}</p>
+                  <hr />
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <p class="card-text">
+                      <img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="42px" class="weekday-weather-icon"/>
+                    </p>
+                  </div>
+                  <div class="col-6">
+                    <p class="card-text center weekday-weather-temps">
+                      ${Math.round(forecastDay.temp.min)}
+                      <br />
+                      <strong class="high">${Math.round(
+          forecastDay.temp.max
+        )}</strong>
+                    </p>
+                    <hr class="low-over-high" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          <!--day 2 end-->
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "7188b6a77c9693ed94470114f98e8761";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
 
 // load a default
 search("Santa Monica");
